298e456e92e5dce849f15382d61df06399fac09b
Raythestudent-momo <70280810+Raythestudent-momo@users.noreply.github.com>
2021-09-25 11:04:57 -0400

    première heure de travail

---
+++ Git/assets/beauregard_casavant_raymond/script.js
@@ -0,0 +1,6 @@
+let reveil = {
+  subtitle: "réveil",
+  text: `Lors de votre réveil, vous remarquez être dans une clairière verdoyante. 
+  Tout d'un coup, un homme sort du couvert des bois, un couteau à la main et l'air menaçant.
+  Que faites-vous?`,
+};
