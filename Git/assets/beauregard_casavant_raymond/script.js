3e8a48530e695d656277cf001abea867ef612489
Raythestudent-momo <70280810+Raythestudent-momo@users.noreply.github.com>
2021-09-25 12:27:31 -0400

    2e heure de travail

--- Git/assets/beauregard_casavant_raymond/script.js
+++ Git/assets/beauregard_casavant_raymond/script.js
@@ -3,4 +3,36 @@ let reveil = {
   text: `Lors de votre réveil, vous remarquez être dans une clairière verdoyante. 
   Tout d'un coup, un homme sort du couvert des bois, un couteau à la main et l'air menaçant.
   Que faites-vous?`,
+  img: "images/steven_reveil.png",
+};
+
+let slave = {
+  subtitle: "réduit/e en esclavage!",
+  text: `Totalement immunisé contre votre charme, Steven vous attrape par la gorge 
+    et vous fait perdre connaissance. Vous vous réveillez dans une cage, menottes
+    aux poignets et aux pieds, en route vers un marché d'esclaves.`,
+  img: "images/slave_ending.png",
+};
+
+let insulter = {
+  subtitle: "sauvé par l'audace",
+  text: `Face à votre audace, Steven vous enrôle pour assassiner un prince 
+  d'une région avoisinante. En souriant, il vous pointe la voie à suivre
+  du bout de son couteau.`,
+  img: "images/steven_2.jpg",
+};
+
+let discuter = {
+  subtitle: "la prudence est de mise...",
+  text: `Vous demandez qui il est. Steven soupire et répond avec un 
+    sourire en coin :
+    - Tu es bien prudente, mais nous ferons connaissance bientôt...
+    et disparaît aussi soudainement qu'il est arrivé.`,
+  img: "",
+};
+
+let chaptersObj = {
+  le_reveil: reveil,
+  slave_ending: slave,
+  insult: insulter,
 };
